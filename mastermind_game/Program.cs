cfa3817080073e9cd029fbc3f22b4433d9e9c252
Marek Mikitovic <__>
2022-02-03 11:44:01 +0100

    Finalised structure

--- mastermind_game/Program.cs
+++ mastermind_game/Program.cs
@@ -5,12 +5,12 @@ namespace mastermind_game
 {
     class Program
     {
-        static int[] secretCode;
-        static int[] guessedCode;
-
         static int maxPegs = 6;
         static int codeLenght = 4;
         static bool gameEnd = false;
+
+        static int[] secretCode = new int[codeLenght];
+        static int[] guessedCode = new int[codeLenght];
 
         static Random rnd = new Random();
 
@@ -23,18 +23,20 @@ namespace mastermind_game
                 ReadCode();
                 CheckCode();
             } while (gameEnd);
+
+            Console.WriteLine("You have breaked the secret code! " + secretCode.ToString());
         }
 
         private static void Setup()
         {
-            Console.WriteLine("Master mind game!/n");
-            // generate rand secret code
-
+            Console.WriteLine("Master mind game!\n");
+            //TODO: fix code generation
             do
             {
                 int num = rnd.Next(1, maxPegs);
                 if(!secretCode.Contains(num))
                     secretCode.Append(num);
+                Console.WriteLine(num);
 
             } while (secretCode.Length < codeLenght);
             
@@ -42,12 +44,37 @@ namespace mastermind_game
 
         private static void CheckCode()
         {
-            //compare codes
+            string feedback = "";
+            int correctPeg = 0; 
+            int guessedPeg = 0;
+
+            if (guessedCode.SequenceEqual(secretCode))
+                gameEnd = true;
+
+            for (int i =01; i < codeLenght-1; i++)
+            {
+                if (guessedCode[i].Equals(secretCode[i]))
+                    correctPeg++;
+                else if (guessedCode.Contains(secretCode[i]))
+                    guessedPeg++;
+            }
+
+            for (int i = 0; i <= correctPeg; i++)
+            {
+                feedback += "Ø";
+            }
+
+            for (int i = 0; i <= guessedPeg; i++)
+            {
+                feedback += "O";
+            }
+
+            Console.Write(" feedback: " + feedback);
         }
 
         private static void ReadCode()
         {
-            Console.WriteLine("Type {0} digit code/n",codeLenght);
+            Console.WriteLine("Type {0} digit code\n",codeLenght);
             int temp = int.Parse(Console.ReadLine());
             while(temp > 0)
             {
